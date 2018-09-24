cb62c797826a7d9e322cef0e87dbb18edbc505ab
Anu Malm <nurkkacthulhu@gmail.com>
2018-09-24 23:15:18 +0300

    Add javadoc to CalculatorWindow-class.
    Basic javadoc added to the CalculatorWindow-class.

--- source/CalculatorWindow.java
+++ source/CalculatorWindow.java
@@ -1,5 +1,10 @@
+/**
+ * @author      Anu Malm     <anu.malm@cs.tamk.fi>
+ * @version     2018.0924
+ * @since       2018.0924
+ */
 public class CalculatorWindow extends JFrame {
     public CalculatorWindow() {
-        
+
     }
 }
