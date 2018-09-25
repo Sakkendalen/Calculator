1b6b3eb3fa2a07c044f8937eab03b1b829bab327
Anu Malm <nurkkacthulhu@gmail.com>
2018-09-25 22:20:16 +0300

    CalcWindow-constructor: add JButton "one",
    Add one button to the CalcWindow constructor. Text on button is currently "1".

--- source/CalcWindow.java
+++ source/CalcWindow.java
@@ -5,12 +5,19 @@ import javax.swing.*;
  * @since       2018.0924
  */
 public class CalcWindow extends JFrame {
+    private JButton one;
     /**
      * Constructor for class CalculatorWindow.
      */
     public CalcWindow() {
+
+        one = new JButton("1");
+
+        add(one);
+        
         setSize(350,500);
         setVisible(true);
+
         // Ends the program when user closes the window
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
