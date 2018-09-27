2c750e5a384df1041fa19398b2445b36200fd554
Essi Supponen <essi.supponen@cs.tamk.fi>
2018-09-27 23:05:20 +0300

    Functions.java: add basic methods
    Added methods add(), multiply(), substract() and divide(). No code yet in any of the methods. More methods will be added later.

--- source/Functions.java
+++ source/Functions.java
@@ -1,3 +1,17 @@
-public static class Functions {
-    
+public class Functions {
+    public static double add(double... numbers) {
+        return 0;
+    }
+
+    public static double multiply(double... numbers) {
+        return 0;
+    }
+
+    public static double substract(double originalNumber, double... numbers) {
+        return 0;
+    }
+
+    public static double divide(double number, double dividor) {
+        return 0;
+    }
 }
