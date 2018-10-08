c559417c9f3294b858de033f66f33bbcdc905ced
Mikko Mustasaari <mikko.mustasaari@cs.tamk.fi>
2018-10-08 18:40:43 +0300

    ScreenToResult: Add calculate() method
    Calculate method added & added return String

--- source/ScreenToResult.java
+++ source/ScreenToResult.java
@@ -1,11 +1,18 @@
 public class ScreenToResult {
 
     Functions functions;
+    String result;
 
     public ScreenToResult() {
 
         functions = new Functions();
 
+    }
+
+    public String calculate(String input) {
+
+
+        return result;
     }
 
 }
